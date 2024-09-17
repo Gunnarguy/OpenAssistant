f459e684288f77e7850d87445e0fad43e052d2f0
gunnarguy <gunnarguy@me.com>
2024-09-16 20:43:22 -0700

    Initial Commit

---
+++ OpenAssistant/OpenAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAssistantApp.swift
+//  OpenAssistant
+//
+//  Created by Gunnar Hostetler on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
