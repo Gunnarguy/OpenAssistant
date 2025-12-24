bd4f73a66c7dd1244f01aa0275a5d6af428ca91e
gunnarguy <gunnarguy@me.com>
2025-12-23 21:53:09 -0800

    feat: add AppDelegate for Firebase configuration

---
+++ AppDelegate.swift
@@ -0,0 +1,11 @@
+import UIKit
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        // Configure Firebase as early as possible
+        FirebaseApp.configure()
+        return true
+    }
+}
